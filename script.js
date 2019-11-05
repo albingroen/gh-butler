7332ba8fcfd83ced33b1200ad9dc80da93511539
Albin Groen <albin.groen@gmail.com>
2019-11-05 22:18:18 +0100

    🎉 Custom templates

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function setup() {
   ];
 
   inquirer.prompt(questions).then(answers => {
-    const token = answers["accessToken"];
+    const token = answers.accessToken;
     if (token.length === 40) {
       fs.writeFileSync(
         `${__dirname}/config.js`,
@@ -78,57 +78,85 @@ function main(githubToken) {
           type: "list",
           name: "template",
           message: "Use a template:",
-          choices: ["None", "React.js", "Node.js", "Node.js + Express", "NPM module"]
+          choices: [
+            "None",
+            "React.js",
+            "Node.js",
+            "Node.js + Express",
+            "NPM module",
+            "Custom"
+          ]
         }
       ];
 
       inquirer.prompt(questions).then(answers => {
-        const useTemplate = answers["template"] !== "None";
-        const templates = {
-          "React.js": "enhanced-cra",
-          "Node.js": "Node-template",
-          "Node.js + Express": "Express-template",
-          "NPM module": "create-npm-module"
-        };
+        if (answers.template === "Custom") {
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "customTemplate",
+                message: "Custom template (github template url):"
+              }
+            ])
+            .then(answers2 => {
+              customTemplate = answers2.customTemplate;
 
-        const postOptions = {
-          method: "POST",
-          url: useTemplate
-            ? `${baseUrl}/repos/albingroen/${
-                templates[answers["template"]]
-              }/generate`
-            : `${baseUrl}/user/repos`,
-          headers: {
-            "User-Agent": "gh-butler",
-            Accept: "application/vnd.github.baptiste-preview+json",
-            Authorization: `token ${githubToken}`
-          },
-          body: {
-            name: answers["name"],
-            description: answers["description"],
-            private: !answers["public"],
-            gitignore_template: !useTemplate ? "Node" : undefined,
-            owner: useTemplate ? "albingroen" : undefined
-          },
-          json: true
-        };
+              continueScript(customTemplate);
+            });
+        } else {
+          continueScript();
+        }
 
-        log(` Creating ${answers["name"]}...`);
+        function continueScript(customTemplate) {
+          const useTemplate = answers.template !== "None";
+          const templates = {
+            "React.js": "enhanced-cra",
+            "Node.js": "Node-template",
+            "Node.js + Express": "Express-template",
+            "NPM module": "create-npm-module"
+          };
 
-        request(postOptions, (error, response, body) => {
-          if (error) console.log({ error });
+          const postOptions = {
+            method: "POST",
+            url: useTemplate
+              ? answers.template !== "Custom"
+                ? `${baseUrl}/repos/albingroen/${
+                    templates[answers.template]
+                  }/generate`
+                : `${baseUrl}/repos${customTemplate.split(".com")[1]}/generate`
+              : `${baseUrl}/user/repos`,
+            headers: {
+              "User-Agent": "gh-butler",
+              Accept: "application/vnd.github.baptiste-preview+json",
+              Authorization: `token ${githubToken}`
+            },
+            body: {
+              name: answers.name,
+              description: answers.description,
+              private: !answers.public,
+              owner: useTemplate ? "albingroen" : undefined
+            },
+            json: true
+          };
 
-          log(` Downloading...`);
-          exec(
-            `cd ~/Documents && git clone ${body.ssh_url} && cd ${body.name} && code .`,
-            error => {
-              if (error) console.log({ error });
-              log(" Opening VSCode...");
-              console.log("");
-              log(" Done", "success");
-            }
-          );
-        });
+          log(` Creating ${answers.name}...`);
+
+          request(postOptions, (error, response, body) => {
+            if (error) console.log({ error });
+
+            log(` Downloading...`);
+            exec(
+              `cd ~/Documents && git clone ${body.ssh_url} && cd ${body.name} && code .`,
+              error => {
+                if (error) console.log({ error });
+                log(" Opening VSCode...");
+                console.log("");
+                log(" Done", "success");
+              }
+            );
+          });
+        }
       });
     });
   }
